c29cb82661d1ebd38b6837c0c2abb30d163718a1
Manikandan-16 <Manikandan-16@users.noreply.github.com>
2018-06-07 20:40:07 +0530

    2nd Commit

---
+++ src/main/java/com/mathproj/MathProject/App.java
@@ -0,0 +1,13 @@
+package com.mathproj.MathProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
